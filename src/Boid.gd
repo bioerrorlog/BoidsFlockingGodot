b1d08ab4501a0d49d5fd4ab1637ffac15e4560ba
bioerrorlog <n.iijima.0x01@gmail.com>
2020-07-12 10:45:17 +0900

    edit params

--- src/Boid.gd
+++ src/Boid.gd
@@ -21,7 +21,7 @@ export (Array, Color) var colors
 
 func _ready():
 	randomize()
-	position = Vector2(rand_range(-800, 800), rand_range(-300, 300))
+	position = Vector2(rand_range(-800, 800), rand_range(-400, 400))
 	velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * move_speed
 	modulate = colors[rand_range(0, colors.size())]
 
